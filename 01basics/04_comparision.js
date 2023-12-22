da3ef4a1dfcc5571b66842fa5b3c8671cba9b9c7
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-22 06:51:54 +0530

    comparision between differet datatypes

---
+++ 01basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/* don't write codes like this
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+*/
+
+// === also checks equality of datatypes
+// == only checks value
+
+console.log("2" === 2);
