5eb64cbba0e59f4d2ca5b62e785b077d91dfafbc
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-23 06:37:58 +0530

    dates and time

---
+++ 01basics/07_datesinJs.js
@@ -0,0 +1,25 @@
+const myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// const myCreatedDate = new Date(2003, 4, 18);
+// const myCreatedDate = new Date(2003, 4, 18, 5, 3);
+// let mineDate = new Date("2003-05-18");
+// console.log(mineDate.toLocaleString());
+
+// console.log(myDate.getTime());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+// console.log(Date.now() / 1000);
+
+// console.log(Math.floor(Date.now() / 1000));
+
+let formedDate = new Date();
+console.log(formedDate);
+console.log(formedDate.getMonth() + 1);
+console.log(formedDate.getDay());
