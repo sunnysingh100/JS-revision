c58597a6cd2e7e71ce8758f3c0c1ffe866ad1c20
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-23 16:54:29 +0530

    array-1

---
+++ 02basics/01_array.js
@@ -0,0 +1,41 @@
+const myArr = [56, 37, 28, 288, 78, 4678];
+// console.log(myArr[2]);
+
+// const myNewArr = new Array(23, 33, 32, 90);
+// console.log(myNewArr[1]);
+
+// myArr.push(44);
+// myArr.pop();
+// myArr.unshift(67);
+// myArr.shift();
+// console.log(myArr.includes(0));
+// console.log(myArr.indexOf(288));
+
+/*
+const myNewArr = myArr.join();
+console.log(myArr);
+console.log(myNewArr);
+console.log(typeof myArr);
+console.log(typeof myNewArr);
+*/
+
+// slice and splice
+// console.log(myArr);
+// console.log("A ", myArr);
+
+console.log(myArr);
+const myn1 = myArr.slice(1, 3);
+console.log(myn1);
+
+console.log(myArr);
+const myn2 = myArr.splice(1, 3);
+console.log(myn2);
+// console.log("B ", myArr);
+// console.log(myArr);
+// console.log(myn1);
+
+// const myn2 = myArr.splice(1, 3);
+// console.log(myArr);
+// console.log("C ", myArr);
+
+// console.log(myn2);
