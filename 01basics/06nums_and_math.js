6926b3e930fe11226b2e9ba02080bc891b70e95a
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-23 05:41:46 +0530

    math and num

---
+++ 01basics/06nums_and_math.js
@@ -0,0 +1,34 @@
+const score = 779;
+// console.log(score);
+
+const balance = new Number(100.76567899383736);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 788.678647;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// ++++++++++++++++ Maths +++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(24.45));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(3, 5, 7, 9));
+// console.log(Math.max(3, 5, 7, 9));
+
+// console.log(Math.floor(Math.random() * 6 + 1));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
