09c8df15853f2018e9da12bd06c13c0a7c711a56
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-25 05:24:49 +0530

    destructuring and json

--- 02basics/04_objects.js
+++ 02basics/04_objects.js
@@ -26,9 +26,21 @@ const obj2 = {3: "c", 4: "d"};
 const obj3 = Object.assign({}, obj1, obj2);
 // console.log(obj3);
 
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLogged"));
+// console.log(tinderUser.hasOwnProperty("isLogged"));
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++  */
+const course = {
+  courseName: "JS-mastery",
+  price: "999",
+  courseInstructor: "Sam tu",
+};
+
+// console.log(course.courseInstructor);
+
+const {courseInstructor: instructor} = course;
+console.log(instructor);
