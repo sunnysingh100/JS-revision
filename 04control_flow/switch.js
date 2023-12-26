3cae71de38d69e22a0a9869927a39df0ef71ad0d
sunnysingh100 <sunnysingh706050@gmail.com>
2023-12-26 07:18:31 +0530

    switch statement

---
+++ 04control_flow/switch.js
@@ -0,0 +1,16 @@
+const month = "mar";
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("Feb");
+    break;
+  case "mar":
+    console.log("March");
+    break;
+
+  default:
+    console.log("nothing default");
+    break;
+}
