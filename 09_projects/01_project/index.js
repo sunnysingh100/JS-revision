d939d294bc91019128abbb4fc7ea69e87ed4938a
sunnysingh100 <sunnysingh706050@gmail.com>
2024-01-02 07:37:21 +0530

    clean up the index.js file

--- 09_projects/01_project/index.js
+++ 09_projects/01_project/index.js
@@ -25,19 +25,3 @@
 // stop.addEventListener("click", function () {
 //   clearInterval(startInterval);
 // });
-
-/* ************************* Second way  ********************** */
-
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-const mainBody = document.querySelector("body");
-
-const randomColor = function () {
-  const hex = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += hex[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-console.log(randomColor());
